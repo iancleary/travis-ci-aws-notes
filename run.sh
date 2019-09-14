4f4a4a4de5b34b500c1f6be192361a2cb4654afd
iancleary <iancleary@pm.me>
2019-09-14 15:03:15 -0700

    adjusting for AMI instance

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 cd /home/ec2-user/travis-ci-aws-flask-demo-folder
 export FLASK_APP=hello.py
 export FLASK_ENV=development
-flask run
+sudo yum install python36 python36-pip -y # could be in ansible
+pip install -r requirements.txt # could be in venv or just a don't care
+python3 -m flask run
