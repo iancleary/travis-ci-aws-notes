a5fbf03918e3bce70ab00c233aedd4885dec92ca
iancleary <iancleary@pm.me>
2019-09-14 15:06:06 -0700

    updated and cleaned file

--- run.sh
+++ run.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+
+# Change from /opt/... to directory specified in appspec.yml
 cd /home/ec2-user/travis-ci-aws-flask-demo-folder
+
+# install python
+sudo yum install python36 python36-pip -y # could be in ansible
+
+#Download get-pip to current directory. It won't install anything, as of now
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+
+# install requirements.txt
+pip3.6 install -r requirements.txt # could be in venv or just a don't care
+
+# run Flask app
 export FLASK_APP=hello.py
 export FLASK_ENV=development
-sudo yum install python36 python36-pip -y # could be in ansible
-pip install -r requirements.txt # could be in venv or just a don't care
 python3 -m flask run
